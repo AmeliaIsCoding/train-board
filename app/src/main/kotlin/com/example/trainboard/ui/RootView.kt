8212f725ae50c055e4b16c86c5a4547147776ccb
Lancelot Liu <lancelot.liu@softwire.com>
2025-07-08 11:45:19 +0100

    wip: Hook onto `MainActivity`.
    Co-authored-by: Nickleby Simmonds <nickleby.simmonds@softwire.com>

---
+++ app/src/main/kotlin/com/example/trainboard/ui/RootView.kt
@@ -0,0 +1,8 @@
+package com.example.trainboard.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RootView(modifier: Modifier = Modifier) {
+}

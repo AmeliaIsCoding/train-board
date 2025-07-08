1f92d17728620a1203521ddcc70e5894b1ba1862
Viktoriya Hristova <Viktoriya.Hristova@softwire.com>
2025-07-08 16:55:23 +0100

    feature: button to LNER site and global state vars

--- app/src/main/java/com/example/trainboard/MainActivity.kt
+++ app/src/main/java/com/example/trainboard/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.trainboard
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.emptyLongSet
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -19,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,7 +38,10 @@ import com.example.trainboard.ui.theme.TrainBoardTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-
+import java.util.SimpleTimeZone
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.net.toUri
 
 
 class MainActivity : ComponentActivity() {
@@ -41,44 +52,80 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrainBoardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                            .background(color = Color.Red),
-                        verticalArrangement = Arrangement.Top,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ){
-                        Text("LNER", color = Color.White, fontSize = 25.sp, modifier = Modifier.padding(10.dp))
-                        Row (){
-                            SimpleExposedDropdown("From")
-                            SimpleExposedDropdown("To")
-
-                        }
-                    }
-
-
-
-
-
+                    Page(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
+@Composable
+fun Page(modifier: Modifier)
+{
+
+    val selectedOptionTextFrom = remember { mutableStateOf("")}
+    val selectedOptionTextTo = remember { mutableStateOf("")}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("LNER", color = Color.White, fontSize = 25.sp, modifier = Modifier.padding(top = 0.dp))
+        Row (){
+            SimpleExposedDropdown("From", selectedOptionTextFrom)
+            SimpleExposedDropdown("To", selectedOptionTextTo)
+
+        }
+        ButtonFromTo(selectedOptionTextFrom, selectedOptionTextTo)
+    }
+
+}
+@Composable
+fun ButtonFromTo (selectedFrom: MutableState<String>, selectedTo: MutableState<String>)
+{   val context = LocalContext.current
+    val buttonText = remember { mutableStateOf("Find route") }
+    val codeDictionary = mutableMapOf("London" to "KGX", "Edinburgh" to "EDB", "Oxford" to "OXF", "Bristol" to "BRI", "Liverpool" to "LVC")
+    val url = "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/${codeDictionary[selectedFrom.value]}/${codeDictionary[selectedTo.value]}/#tab_livedepartures"
+    Button(
+
+        onClick = {
+
+            if (selectedFrom.value != "" && selectedTo.value != "")
+            {
+                val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+
+                context.startActivity(intent)
+            }
+            else {
+
+                buttonText.value = "Stations not chosen"
+            }
+
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        )
+    )
+    {
+        Text(buttonText.value)
+    }
+
+
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleExposedDropdown(name: String) {
+fun SimpleExposedDropdown(name: String, selectedOptionText: MutableState<String>) {
     val options = listOf("London", "Edinburgh", "Oxford", "Liverpool", "Bristol")
     var expanded by remember { mutableStateOf(false) }
-    var selectedOptionText by remember { mutableStateOf(options[0]) }
-
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = !expanded }
     ) {
         TextField(
-            value = selectedOptionText,
+            value = selectedOptionText.value,
             onValueChange = {},
             readOnly = true,
             label = { Text(name) },
@@ -96,7 +143,7 @@ fun SimpleExposedDropdown(name: String) {
                 DropdownMenuItem(
                     text = { Text(selectionOption) },
                     onClick = {
-                        selectedOptionText = selectionOption
+                        selectedOptionText.value = selectionOption
                         expanded = false
                     }
                 )

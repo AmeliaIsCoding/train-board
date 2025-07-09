1eda17cd146d90b0fef2fac4cac0b377efa3565b
Viktoriya Hristova <Viktoriya.Hristova@softwire.com>
2025-07-09 09:59:20 +0100

    feature: you cannot choose the same station from the two dropdown menus

--- app/src/main/java/com/example/trainboard/MainActivity.kt
+++ app/src/main/java/com/example/trainboard/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.trainboard
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.collection.emptyLongSet
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
@@ -38,9 +34,7 @@ import com.example.trainboard.ui.theme.TrainBoardTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import java.util.SimpleTimeZone
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat.startActivity
 import androidx.core.net.toUri
 
 
@@ -52,18 +46,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrainBoardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Page(modifier = Modifier.padding(innerPadding))
+                    Page()
                 }
             }
         }
     }
 }
 @Composable
-fun Page(modifier: Modifier)
+fun Page()
 {
+
+
 
     val selectedOptionTextFrom = remember { mutableStateOf("")}
     val selectedOptionTextTo = remember { mutableStateOf("")}
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -73,8 +70,8 @@ fun Page(modifier: Modifier)
     ){
         Text("LNER", color = Color.White, fontSize = 25.sp, modifier = Modifier.padding(top = 0.dp))
         Row (){
-            SimpleExposedDropdown("From", selectedOptionTextFrom)
-            SimpleExposedDropdown("To", selectedOptionTextTo)
+            SimpleExposedDropdown("From", selectedOptionTextFrom, selectedOptionTextTo)
+            SimpleExposedDropdown("To", selectedOptionTextTo, selectedOptionTextFrom)
 
         }
         ButtonFromTo(selectedOptionTextFrom, selectedOptionTextTo)
@@ -117,7 +114,7 @@ fun ButtonFromTo (selectedFrom: MutableState<String>, selectedTo: MutableState<S
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SimpleExposedDropdown(name: String, selectedOptionText: MutableState<String>) {
+fun SimpleExposedDropdown(name: String, selectedOptionText: MutableState<String>,selectedOtherOptionText: MutableState<String>) {
     val options = listOf("London", "Edinburgh", "Oxford", "Liverpool", "Bristol")
     var expanded by remember { mutableStateOf(false) }
     ExposedDropdownMenuBox(
@@ -140,13 +137,16 @@ fun SimpleExposedDropdown(name: String, selectedOptionText: MutableState<String>
             onDismissRequest = { expanded = false }
         ) {
             options.forEach { selectionOption ->
-                DropdownMenuItem(
-                    text = { Text(selectionOption) },
-                    onClick = {
-                        selectedOptionText.value = selectionOption
-                        expanded = false
-                    }
-                )
+                if (selectionOption != selectedOtherOptionText.value){
+                    DropdownMenuItem(
+                        text = { Text(selectionOption) },
+                        onClick = {
+                            selectedOptionText.value = selectionOption
+                            expanded = false
+
+                        }
+                    )
+                }
             }
         }
     }
